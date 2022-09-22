7bbe1341e74a7e7ad5896f31fc8515037f59f483
Molnar Tamas <tamas.molnar.801001@gmail.com>
2022-09-22 17:39:09 +0200

    add swagger ui

--- jobsearch/src/main/java/com/example/jobsearch/controllers/PositionController.java
+++ jobsearch/src/main/java/com/example/jobsearch/controllers/PositionController.java
@@ -7,14 +7,16 @@ import com.example.jobsearch.services.PositionService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/api")
 public class PositionController {
 
   private final PositionService positionService;
