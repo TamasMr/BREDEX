5d842dd638dcb4fc0e997a6d59f520707f5e34a9
Molnar Tamas <tamas.molnar.801001@gmail.com>
2022-09-22 18:51:52 +0200

    costumize swagger io further

--- jobsearch/src/main/java/com/example/jobsearch/JobsearchApplication.java
+++ jobsearch/src/main/java/com/example/jobsearch/JobsearchApplication.java
@@ -5,14 +5,17 @@ import com.example.jobsearch.models.Position;
 import com.example.jobsearch.repositories.ClientRepository;
 import com.example.jobsearch.repositories.PositionRepository;
 import java.net.URL;
+import java.util.Collections;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -39,6 +42,15 @@ public class JobsearchApplication implements ApplicationRunner {
     return new Docket(DocumentationType.SWAGGER_2)
         .select()
         .apis(RequestHandlerSelectors.basePackage("com.example.jobsearch"))
+        .build()
+        .apiInfo(apiDetails());
+  }
+
+  private ApiInfo apiDetails() {
+    return new ApiInfoBuilder()
+        .title("Job Search API")
+        .version("1.0.0")
+        .description("upload and/or search jobs")
         .build();
   }
 
